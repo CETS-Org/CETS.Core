4898e19daaab1d2c1daabc8250660dda3d24bd74
Minhaza <minhazaaa1006@gmail.com>
2025-08-29 21:12:15 +0700

    feat: update unique email

---
+++ Infrastructure/Migrations/20250829104832_AddUniqueEmail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "UQ_IDN_Accounts_Email",
+                table: "IDN_Accounts",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_IDN_Accounts_Email",
+                table: "IDN_Accounts");
+        }
+    }
+}
