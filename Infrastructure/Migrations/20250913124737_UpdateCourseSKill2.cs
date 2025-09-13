428a0084e22bb078285530600be8f597792ba87b
Nguyễn Minh Trung <trungnmde170101@fpt.edu.vn>
2025-09-13 19:48:19 +0700

    fix : update CourseSkill Entity

---
+++ Infrastructure/Migrations/20250913124737_UpdateCourseSKill2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCourseSKill2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CourseBenefitID",
+                table: "ACAD_CourseSkills",
+                newName: "CourseSkillID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CourseSkillID",
+                table: "ACAD_CourseSkills",
+                newName: "CourseBenefitID");
+        }
+    }
+}
