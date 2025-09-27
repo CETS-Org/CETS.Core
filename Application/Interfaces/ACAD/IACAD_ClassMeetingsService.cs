0dd85fb1732f01a2f25d3829a59736cfb920dee4
duongphan <duong.pnt@savvycom.vn>
2025-09-27 14:10:17 +0700

    feat : view student learning schedule repo_service

---
+++ Application/Interfaces/ACAD/IACAD_ClassMeetingsService.cs
@@ -0,0 +1,14 @@
+﻿using DTOs.ACAD.ACAD_ClassMeetings.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.ACAD
+{
+    public interface IACAD_ClassMeetingsService
+    {
+        Task<IEnumerable<StudentWeeklyScheduleResponse>> WeeklyScheduleGetByStudentAsync(Guid studentId, CancellationToken ct);
+    }
+}
--- Domain/Interfaces/ACAD/IACAD_ClassMeetingRepository.cs
+++ Domain/Interfaces/ACAD/IACAD_ClassMeetingRepository.cs
@@ -1,10 +1,12 @@
 using Domain.Entities;
+using DTOs.ACAD.ACAD_ClassMeetings.Responses;
 
 namespace Domain.Interfaces.ACAD
 {
     public interface IACAD_ClassMeetingRepository : IBaseRepository<ACAD_ClassMeeting>
     {
         Task<ACAD_ClassMeeting?> GetClassMeetingTodayByClassId(Guid classId);
+        Task<IEnumerable<StudentWeeklyScheduleResponse>> WeeklyScheduleGetByStudentAsync(Guid studentId, CancellationToken ct);
     }
 }
 
