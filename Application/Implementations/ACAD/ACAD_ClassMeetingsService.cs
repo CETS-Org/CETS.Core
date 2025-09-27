f98e1ea2b0023dd9e3053f84e2ef216417b780da
duongphan <duong.pnt@savvycom.vn>
2025-09-27 14:10:17 +0700

    feat : view student learning schedule repo_service

---
+++ Application/Implementations/ACAD/ACAD_ClassMeetingsService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces.ACAD;
+using Domain.Interfaces.ACAD;
+using DTOs.ACAD.ACAD_ClassMeetings.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Implementations.ACAD
+{
+    public class ACAD_ClassMeetingsService : IACAD_ClassMeetingsService
+    {
+        private readonly IACAD_ClassMeetingRepository _classMeetingRepository;
+        public ACAD_ClassMeetingsService(IACAD_ClassMeetingRepository classMeetingRepository)
+        {
+            _classMeetingRepository = classMeetingRepository;
+        }
+        public async Task<IEnumerable<StudentWeeklyScheduleResponse>> WeeklyScheduleGetByStudentAsync(Guid studentId, CancellationToken ct)
+        {
+            return await _classMeetingRepository.WeeklyScheduleGetByStudentAsync(studentId, ct);
+        }
+    }
+}
