f2499dec94ee20c2e2af67c004d7ea47ff9fc03f
Minhaza <minhazaaa1006@gmail.com>
2025-09-30 22:20:44 +0700

    feat: add phone number in log in response

--- DTOs/IDN/IDN_Account/Responses/LoginAccountResponse.cs
+++ DTOs/IDN/IDN_Account/Responses/LoginAccountResponse.cs
@@ -15,6 +15,7 @@ namespace DTOs.IDN.IDN_Account.Responses
         public Guid Id { get; set; }
         public string? Email { get; set; }
         public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
         public List<string> RoleNames { get; set; } = new();
         public string? AvatarUrl { get; set; }
         public bool IsVerified { get; set; }
