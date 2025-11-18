8bf568dd46047cfb136cd522d6654beaffa9c901
Minhaza <minhazaaa1006@gmail.com>
2025-11-18 17:45:12 +0700

    update: Update MongoNotificationOption

--- Infrastructure/Implementations/Common/MongoDB/MongoNotificationOptions.cs
+++ Infrastructure/Implementations/Common/MongoDB/MongoNotificationOptions.cs
@@ -4,6 +4,6 @@ public class MongoNotificationOptions
 {
     public const string SectionName = "Mongo:Notification";
     public string? ConnectionString { get; set; }
-    public string Database { get; set; } = null!;
+    public string? Database { get; set; }
     public string Collection { get; set; } = "notifications";
 }
